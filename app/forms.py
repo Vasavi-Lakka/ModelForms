81c30038c3333e74cdf9837aee6791004095ac38
Your Name <vasavilakka398@gmail.com>
2024-10-28 17:56:06 +0530

    first commit

--- app/forms.py
+++ app/forms.py
@@ -11,8 +11,12 @@ class WebpageModelForm(forms.ModelForm):
     class Meta:
         model=Webpage
         fields='__all__'
-
-class AccessModelForm(forms.ModelForm):
-    class Meta:
-        model=AccessRecord
-        fields="__all__"
+        # fields=['topic_name', 'name']
+        #exclude=['url']
+        help_texts={'topic_name':'shouldnot be num'}
+        labels={'topic_name':'TN'}
+        widgets={'url':forms.PasswordInput}
+# class AccessModelForm(forms.ModelForm):
+#     class Meta:
+#         model=AccessRecord
+#         fields="__all__"
